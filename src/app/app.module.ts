6db5e3ca74db4f838a86e8a53e7122b4a4c7dd40
Yaroslav Solonnikov <yaroslav.solonnikov@gmail.com>
2018-02-07 20:43:13 +0200

    added add-client form with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
@@ -40,6 +40,7 @@ import { ClientService } from './services/client.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase, 'clientpanel2'),
     AngularFirestoreModule,
     AngularFireAuthModule
